bc102af6ad1be16811d046f97969cacaea682d58
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-06-08 19:29:48 +0200

    New Bubu app

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,45 +5,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send(generateStudents());
+    res.send(generateSentence());
 });
 
 app.listen(3000, function() {
     console.log('Accepting HTTP requests on port 3000.');
 });
 
-// Generation d'etudiants aleatoires
-function generateStudents() {
-    // On peut aller de 0 à 10 etudiants
-    var nbOfStudents = chance.integer({
-        // Set des bornes
-        min: 0,
-        max: 10
-    });
-    // Affichage du nombre d'etudiants
-    console.log(nbOfStudents);
-    // Tableau de resultats
-    var students = [];
-    // Generation de donnees aleatoires
-    for (var i = 0; i < nbOfStudents; ++i) {
-        var gender = chance.gender();
-        var birthYear = chance.year({
-            min: 1985,
-            max: 2000
-        });
-        // Insertion des donnees
-        students.push({
-            firstName: chance.first({
-                // Prenom specifique au genre
-                gender: gender
-            }),
-            lastName: chance.last(),
-            gender: gender,
-            birthday: chance.birthday({
-                year: birthYear
-            })
-        });
-    };
-    console.log(students);
-    return students;
+function generateSentence() {
+    var sentences = [];
+    for (var i = 0; i < 3; ++i) {
+        var newSentence = chance.sentence();
+        sentences.push(newSentence);
+    }
+    console.log(sentences);
+    return sentences;
 }
