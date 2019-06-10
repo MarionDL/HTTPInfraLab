46ceeddd42889fd75abe574794c8d92919334446
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-06-10 17:38:41 +0200

    LB with multiple server nodes

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,16 +1,36 @@
 <?php 
-    $static_app = getenv('STATIC_APP');
-    $dynamic_app = getenv('DYNAMIC_APP');
+    $static_app1 = getenv('STATIC_APP1');
+    $dynamic_app1 = getenv('DYNAMIC_APP1');
+	
+	$static_app2 = getenv('STATIC_APP2');
+    $dynamic_app2 = getenv('DYNAMIC_APP2');
 ?>
 
 <VirtualHost *:80>
 
     ServerName demo.res.ch
+	
+	<Proxy balancer://static-cluster>
+	
+		BalancerMember 'http://<?php print "$static_app1"?>'
+		BalancerMember 'http://<?php print "$static_app2"?>'
 
-    ProxyPass '/api/bubu/' 'http://<?php print "$dynamic_app"?>/'
-    ProxyPassReverse '/api/bubu/' 'http://<?php print "$dynamic_app"?>/'
+	</Proxy>
+	
+	<Proxy balancer://dynamic-cluster>
 
-    ProxyPass '/' 'http://<?php print "$static_app"?>/'
-    ProxyPassReverse '/' '<?php print "$static_app"?>/'
+		BalancerMember 'http://<?php print "$dynamic_app1"?>'
+		BalancerMember 'http://<?php print "$dynamic_app2"?>'
+
+	</Proxy>
+	
+	ProxyPreserveHost On
+	
+	ProxyPass '/api/bubu/' balancer://dynamic-cluster/
+	ProxyPassReverse '/api/bubu/' balancer://dynamic-cluster/
+	
+	ProxyPass '/' balancer://static-cluster/
+	ProxyPassReverse '/' balancer://static-cluster/
+	
 
 </VirtualHost>
